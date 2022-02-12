6a8b25d6994e58d4929a343e8ce3c885ad367a98
baihan <bh198@hotmail.com>
2022-02-12 21:38:24 +0800

    Add compute image

--- src/bin/basic.rs
+++ src/bin/basic.rs
@@ -18,6 +18,9 @@ use vulkano::image::{ImageDimensions, StorageImage};
 use vulkano::format::{Format, ClearValue};
 use image::{ImageBuffer, Rgba};
 
+// For compute image
+use vulkano::image::view::ImageView;
+
 // Submit commands
 use vulkano::sync;
 use vulkano::sync::GpuFuture;
@@ -35,6 +38,7 @@ enum PracticeType
     Simple,
     Compute,
     Image,
+    ComputeImage,
 }
 
 fn main()
@@ -60,7 +64,9 @@ fn main()
 
     let queue = queues.next().unwrap();
 
-    let buffer_type = PracticeType::Image;
+    let buffer_type = PracticeType::ComputeImage;
+
+    #[allow(unreachable_patterns, unused_variables)]
     match buffer_type
     {
         PracticeType::Simple => {
@@ -228,6 +234,118 @@ fn main()
             let showing_image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
             showing_image.save("image.png").unwrap();
         }
+
+        PracticeType::ComputeImage => {
+            let image = StorageImage::new(
+                device.clone(), ImageDimensions::Dim2d {
+                    width: 1024,
+                    height: 1024,
+                    array_layers: 1,
+                }, 
+                Format::R8G8B8A8_UNORM,
+                Some(queue.family()),
+            ).unwrap();
+
+            let view = ImageView::new(image.clone()).unwrap();
+
+
+            mod cs {
+                vulkano_shaders::shader!{
+                    ty: "compute",
+                    src: "
+                        #version 450
+
+                        layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+                        
+                        layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
+                        
+                        void main() {
+                            vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
+                            vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
+                        
+                            vec2 z = vec2(0.0, 0.0);
+                            float i;
+                            for (i = 0.0; i < 1.0; i += 0.005) {
+                                z = vec2(
+                                    z.x * z.x - z.y * z.y + c.x,
+                                    z.y * z.x + z.x * z.y + c.y
+                                );
+                        
+                                if (length(z) > 4.0) {
+                                    break;
+                                }
+                            }
+                        
+                            vec4 to_write = vec4(vec3(i), 1.0);
+                            imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
+                        }                    
+                    "
+                }
+            }
+
+            let shader = cs::load(device.clone())
+                .expect("Failed to create shader module!");
+
+            let compute_pipeline = ComputePipeline::new(
+                device.clone(), 
+                shader.entry_point("main").unwrap(), 
+                &(), 
+                None, 
+                |_| (),
+            ).expect("Failed to create compute pipeline");
+
+            let layout = compute_pipeline
+                .layout()
+                .descriptor_set_layouts()
+                .get(0)
+                .unwrap();
+
+            let set = PersistentDescriptorSet::new(
+                layout.clone(), 
+                [WriteDescriptorSet::image_view(0, view.clone())],
+            ).unwrap();
+
+            let buf = CpuAccessibleBuffer::from_iter(device.clone(), 
+                BufferUsage::all(),
+                false,
+                (0..1024 * 1024 * 4).map(|_| 0u8),
+            ).expect("failed to create buffer");
+
+            let mut builder = AutoCommandBufferBuilder::primary(
+                device.clone(), 
+                queue.family(), 
+                CommandBufferUsage::OneTimeSubmit,
+            ).unwrap();
+
+            builder
+                .bind_pipeline_compute(compute_pipeline.clone())
+                .bind_descriptor_sets(
+                    PipelineBindPoint::Compute,
+                    compute_pipeline.layout().clone(),
+                    0,
+                    set,
+                )
+                .dispatch([1024 / 8, 1024 / 8, 1])
+                .unwrap()
+                .copy_image_to_buffer(image.clone(), buf.clone())
+                .unwrap();
+            
+            let command_buffer = builder.build().unwrap();
+
+            let future = sync::now(device.clone())
+                .then_execute(queue.clone(), command_buffer)
+                .unwrap()
+                .then_signal_fence_and_flush()
+                .unwrap();
+
+            future.wait(None).unwrap();
+
+            let buffer_content = buf.read().unwrap();
+            let out_image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
+            out_image.save("image.png").unwrap();
+        }
+
+        other => {}
     }
 
 }
